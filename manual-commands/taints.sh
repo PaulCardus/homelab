c00aee609f9968f0cc54f4014402072fb4285691
Paul Cardus <cardusuk@outlook.com>
2025-10-30 13:31:14 +0000

    adding redis to homelab

--- manual-commands/taints.sh
+++ manual-commands/taints.sh
@@ -1,2 +1,5 @@
 kubectl taint nodes k8s-monitoring-1 monitoring=true:NoSchedule
 kubectl label nodes k8s-monitoring-1 monitoring=true
+
+kubectl taint nodes k8s-monitoring-1 redis=true:NoSchedule
+kubectl label nodes k8s-monitoring-1 redis=true
